0781771887ac7ea61c5d687d369b95ee2f3ddf53
sfincks <sfincks666@gmail.com>
2023-02-17 15:57:29 +0500

    zadacha_01

--- Zadacha_01/Program.cs
+++ Zadacha_01/Program.cs
@@ -3,3 +3,41 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+int insertDimensionNumber(string welcomeText)
+{
+    System.Console.WriteLine(welcomeText);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] generateMatrix(int m, int n)
+{
+    Random rng = new Random();
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round((rng.NextDouble() * (100 - 10) + 10), 1);
+        }
+    }
+    return array;
+}
+void printMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int m = insertDimensionNumber("Введите значение m");
+int n = insertDimensionNumber("Введите значение n");
+double[,] matrix = generateMatrix(m, n);
+// System.Console.WriteLine("["+string.Join(", ",matrix)+"]");
+System.Console.WriteLine();
+printMatrix(matrix);
